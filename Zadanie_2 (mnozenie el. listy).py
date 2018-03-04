cc62870c1114f185028dbb990388cc3937565e02
Szymon <33368441+SKpracownia1@users.noreply.github.com>
2018-03-04 18:42:48 +0100

    Add files via upload

---
+++ Zadanie_2 (mnozenie el. listy).py
@@ -0,0 +1,21 @@
+#Zadanie 3
+
+print("Zmodyfikuj istniejaca liste mnozac kazdy element listy liczb calkowitych przez 3")
+print("Uzyj rozszerzonej skladni list: [wyr for elem in lista].")
+
+lista=[10,20,30,40,50]
+
+print("Wejscie:"+str(lista))
+
+lista=[3*i for i in lista]
+
+print("Wyjscie:"+str(lista))
+
+
+
+
+
+
+
+
+
